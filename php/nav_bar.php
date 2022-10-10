edeac1f0b8dbaca553cb76bf9cd3acf843b925f1
dannymandelli <dannymandelli@gmail.com>
2022-10-10 03:59:47 -0400

    Split site and create some univeral files to call into

---
+++ php/nav_bar.php
@@ -0,0 +1,40 @@
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="/style.css" >
+
+<nav id="navbar-placeholder" class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container">
+    <a class="navbar-brand brand_name" href="/"><h1>Daniel Mandelli</h1></a>
+    <div class="social-part">
+      <a href="https://www.linkedin.com/in/daniel-mandelli/" target="_blank" rel="noreferrer noopener"><i class="fa fa-linkedin"></i></a>
+      <a href="https://www.facebook.com/danny.mandelli" target="_blank" rel="noreferrer noopener"><i class="fa fa-facebook"></i></a>
+    </div>
+    <button class="navbar-toggler" type="button" id="navbar-toggler" data-toggle="collapse"
+    data-target="#navbarResponsive" aria-controls="navbarResponsive"
+    aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon active"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="nav navbar-nav mr-auto" id="navbar_list">
+        <li class="nav-item">
+          <a class="nav-link" href="/pages/education.html">EDUCATION</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/pages/experience.html">EXPERIENCE</a>
+        </li>
+      </ul>
+      <ul class="nav navbar-nav ml-auto">
+        <li class= "nav-item dropdown ">
+          <a class="btn btn-secondary dropdown-toggle" href="#" role="button" aria-controls="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true">
+            Contact Me
+          </a>
+          <div class="dropdown-menu dropdown-menu-right" id="dropdownMenuButton">
+            <a class="dropdown-item" href="mailto:Dannymandelli@gmail.com" target="_blank" rel="noreferrer noopener"><i class="fa fa-solid fa-envelope"></i> <b>Dannymandelli@gmail.com</b></a>
+            <a class="dropdown-item" href="tel:14015722377" target="_blank" rel="noreferrer noopener"><i class="fa fa-phone"></i> <b>Call me at 401-572-2377 </b></a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
